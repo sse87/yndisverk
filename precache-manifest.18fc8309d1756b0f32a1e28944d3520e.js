73fb3b4cbf5f30b11f87e29afb9b47e257ffc38f
Sigurður Snær Eiríksson <sse1987@gmail.com>
2019-04-01 09:55:31 +0000

    Updates

---
+++ precache-manifest.18fc8309d1756b0f32a1e28944d3520e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0b5979198cab0abc38e",
+    "url": "/yndisverk/static/js/runtime~main.9e397a46.js"
+  },
+  {
+    "revision": "263de81ce8846c3bc043",
+    "url": "/yndisverk/static/js/main.a83b5da4.chunk.js"
+  },
+  {
+    "revision": "1957b456bc8866893e8f",
+    "url": "/yndisverk/static/js/2.a0e3560b.chunk.js"
+  },
+  {
+    "revision": "263de81ce8846c3bc043",
+    "url": "/yndisverk/static/css/main.e1bef198.chunk.css"
+  },
+  {
+    "revision": "1957b456bc8866893e8f",
+    "url": "/yndisverk/static/css/2.0167ffe0.chunk.css"
+  },
+  {
+    "revision": "adcfd0726f6f4a3cd29419aabe10a53e",
+    "url": "/yndisverk/index.html"
+  }
+];
