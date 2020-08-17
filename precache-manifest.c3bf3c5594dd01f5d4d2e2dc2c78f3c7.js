7cf05ae7014c6ecd90a61717dafc598d638eb0d6
Sigurður Snær Eiríksson <sse1987@gmail.com>
2020-08-17 15:56:41 +0000

    Updates

---
+++ precache-manifest.c3bf3c5594dd01f5d4d2e2dc2c78f3c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19a4a85411ab69918fb83b410331863",
+    "url": "/matverk/index.html"
+  },
+  {
+    "revision": "635a74250eccb10046b3",
+    "url": "/matverk/static/css/2.afc36f9d.chunk.css"
+  },
+  {
+    "revision": "27ee9c76b4a82b22889d",
+    "url": "/matverk/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "635a74250eccb10046b3",
+    "url": "/matverk/static/js/2.a19ca332.chunk.js"
+  },
+  {
+    "revision": "73556b140791e7f1e95a61af7da7dddb",
+    "url": "/matverk/static/js/2.a19ca332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ee9c76b4a82b22889d",
+    "url": "/matverk/static/js/main.1ec35e9c.chunk.js"
+  },
+  {
+    "revision": "a25c0010f7a1eab26302",
+    "url": "/matverk/static/js/runtime-main.67ad0ac3.js"
+  }
+]);
